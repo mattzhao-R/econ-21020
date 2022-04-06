0746f698879e9f3e7db256172343a93c22ce0d67
Matthew Zhao <matthewhrzhao@gmail.com>
2022-04-05 22:17:04 -0500

    first

---
+++ pset 1 econ 21020.R
@@ -0,0 +1,69 @@
+library(ggplot2)
+set.seed(seed = 1000)
+
+#Problem 8
+
+# Part (a)
+# Generate a vector of n draws from a standard normal rv
+n <- 10000
+mu <- 0
+sigma <- 1
+x <- rnorm(n, mu, sigma)
+
+# Plot a histogram of the draws using ggplot2
+ggplot() + 
+  geom_histogram(aes(x = x), binwidth = 0.16) + 
+  ylab("Count") + xlab("x") + 
+  theme_classic(base_size = 20) + 
+  theme(text = element_text(size = 20,  family="serif"))
+
+# Part (b)
+# Generate a vector of n draws from a uniform(-1, 1) rv
+n <- 10000
+min_y <- -1
+max_y <- 1
+y <- runif(n, min_y , max_y)
+
+# Plot a histogram of the draws using ggplot2
+ggplot() + 
+  geom_histogram(aes(x = y), binwidth = 0.08) + 
+  ylab("Count") + xlab("y") + 
+  theme_classic(base_size = 20) + 
+  theme(text = element_text(size = 20,  family="serif"))
+
+
+# Problem 9
+# Part (a)
+
+
+# Part (b)
+# Define a custom function that returns draws from a Bernoulli rv
+my_rbernoulli <- function (n, p) {
+  x <- ifelse(runif(n,0,1)<=p,0,1)
+  return(x)
+}
+
+# Test the custom Bernoulli generator function
+x <- my_rbernoulli(10000, 0.5)
+length(x) == 10000 # should return TRUE
+mean(x) # should be a number near 0.5
+
+# Part (c)
+# Define a custom function that returns draws from a Binomial rv
+my_rbinomial <- function(n, p, m){
+  for (y in 1:n) {
+    ifelse(y==1,x <- as.vector(sum(my_rbernoulli(m,p))), 
+           x <- append(x,sum(my_rbernoulli(m,p))))
+  }
+  return(x)
+}
+
+# Test the custom Binomial generator function
+x <- my_rbinomial(10000, 0.5, 10)
+length(x) == 10000 # should return TRUE
+mean(x) # should be a number near 5
+
+
+
+
+
